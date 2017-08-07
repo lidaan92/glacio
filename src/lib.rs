19cde3cb7f28da8a763b419eeb7189f718255871
Pete Gadomski <pete.gadomski@gmail.com>
2017-08-06 21:02:53 -0800

    Strip things from lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,23 @@
 extern crate chrono;
-#[macro_use]
-extern crate iron;
-extern crate persistent;
-#[macro_use]
-extern crate router;
-extern crate serde;
-#[macro_use]
-extern crate serde_derive;
-extern crate serde_json;
-extern crate toml;
 extern crate url;
 
-pub mod api;
 mod camera;
 
 pub use camera::{Camera, Image};
 
 #[derive(Debug)]
 pub enum Error {
+    /// Wrapper around `chrono::ParseError`.
     ChronoParse(chrono::ParseError),
-    Io(std::io::Error),
+    /// Wrapper around `std::path::StripPrefixError`.
     StripPrefix(std::path::StripPrefixError),
-    TomlDe(toml::de::Error),
+    /// Wrapper around `url::ParseError`.
     UrlParse(url::ParseError),
-}
-
-impl From<std::io::Error> for Error {
-    fn from(err: std::io::Error) -> Error {
-        Error::Io(err)
-    }
 }
 
 impl From<std::path::StripPrefixError> for Error {
     fn from(err: std::path::StripPrefixError) -> Error {
         Error::StripPrefix(err)
-    }
-}
-
-impl From<toml::de::Error> for Error {
-    fn from(err: toml::de::Error) -> Error {
-        Error::TomlDe(err)
     }
 }
 
@@ -59,9 +37,7 @@ impl std::error::Error for Error {
     fn description(&self) -> &str {
         match *self {
             Error::ChronoParse(ref err) => err.description(),
-            Error::Io(ref err) => err.description(),
             Error::StripPrefix(ref err) => err.description(),
-            Error::TomlDe(ref err) => err.description(),
             Error::UrlParse(ref err) => err.description(),
         }
     }
@@ -71,9 +47,7 @@ impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match *self {
             Error::ChronoParse(ref err) => err.fmt(f),
-            Error::Io(ref err) => err.fmt(f),
             Error::StripPrefix(ref err) => err.fmt(f),
-            Error::TomlDe(ref err) => err.fmt(f),
             Error::UrlParse(ref err) => err.fmt(f),
         }
     }

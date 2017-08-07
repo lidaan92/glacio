cfd5a0026782c5719697f8028c9b569c7080ffc8
Pete Gadomski <pete.gadomski@gmail.com>
2017-08-06 21:16:53 -0800

    Iterator's ok

--- src/camera.rs
+++ src/camera.rs
@@ -1,5 +1,81 @@
+use {Error, Result};
+use std::fs::{DirEntry, ReadDir};
+use std::path::{Path, PathBuf};
+
 #[derive(Debug)]
-pub struct Camera;
+pub struct Camera {
+    directory: PathBuf,
+}
+
+/// An iterator over a camera's images.
+pub struct Images {
+    read_dir: ReadDir,
+}
 
 #[derive(Debug)]
 pub struct Image;
+
+impl Camera {
+    /// Creates a new camera for the provided path.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use glacio::Camera;
+    /// let camera = Camera::new("data/ATLAS_CAM");
+    /// ```
+    pub fn new<P: AsRef<Path>>(path: P) -> Camera {
+        Camera { directory: path.as_ref().to_path_buf() }
+    }
+
+    /// Returns an iterator over this camera's images.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use glacio::Camera;
+    /// let camera = Camera::new("data/ATLAS_CAM");
+    /// let images = camera.images().unwrap().collect::<Vec<_>>();
+    /// ```
+    pub fn images(&self) -> Result<Images> {
+        self.directory
+            .read_dir()
+            .map(|read_dir| Images { read_dir: read_dir })
+            .map_err(Error::from)
+    }
+}
+
+impl Iterator for Images {
+    type Item = Result<Image>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.read_dir.next().map(|result| {
+                                     result.map_err(Error::from).and_then(|dir_entry| {
+                                                                              Image::new(dir_entry)
+                                                                          })
+                                 })
+    }
+}
+
+impl Image {
+    fn new(dir_entry: DirEntry) -> Result<Image> {
+        Ok(Image)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_camera() {
+        let camera = Camera::new("data/ATLAS_CAM");
+    }
+
+    #[test]
+    fn camera_images() {
+        let camera = Camera::new("data/ATLAS_CAM");
+        let images = camera.images().unwrap();
+        assert_eq!(1, images.count());
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -9,10 +9,18 @@ pub use camera::{Camera, Image};
 pub enum Error {
     /// Wrapper around `chrono::ParseError`.
     ChronoParse(chrono::ParseError),
+    /// Wrapper around `std::io::Error`.
+    Io(std::io::Error),
     /// Wrapper around `std::path::StripPrefixError`.
     StripPrefix(std::path::StripPrefixError),
     /// Wrapper around `url::ParseError`.
     UrlParse(url::ParseError),
+}
+
+impl From<std::io::Error> for Error {
+    fn from(err: std::io::Error) -> Error {
+        Error::Io(err)
+    }
 }
 
 impl From<std::path::StripPrefixError> for Error {
@@ -37,6 +45,7 @@ impl std::error::Error for Error {
     fn description(&self) -> &str {
         match *self {
             Error::ChronoParse(ref err) => err.description(),
+            Error::Io(ref err) => err.description(),
             Error::StripPrefix(ref err) => err.description(),
             Error::UrlParse(ref err) => err.description(),
         }
@@ -47,6 +56,7 @@ impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match *self {
             Error::ChronoParse(ref err) => err.fmt(f),
+            Error::Io(ref err) => err.fmt(f),
             Error::StripPrefix(ref err) => err.fmt(f),
             Error::UrlParse(ref err) => err.fmt(f),
         }

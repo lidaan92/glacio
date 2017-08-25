ffa666f5a16305f4a8cc330f8616a9a3e24a9b29
Pete Gadomski <pete.gadomski@gmail.com>
2017-08-25 15:50:25 -0600

    Oh man this is a push

--- src/api/heartbeat.rs
+++ src/api/heartbeat.rs
@@ -9,7 +9,11 @@ pub struct Config {
 }
 
 #[derive(Debug, Serialize)]
-pub struct Status;
+pub struct Status {
+    last_heartbeat_received: String,
+    soc1: f32,
+    soc2: f32,
+}
 
 impl Config {
     pub fn status(&self, request: &Request) -> Result<Status> {
@@ -17,6 +21,12 @@ impl Config {
             .map(|read_sbd| read_sbd.filter_map(|result| result.ok()).collect::<Vec<_>>())
             .unwrap();
         heartbeats.sort_by(|a, b| b.cmp(&a));
-        Ok(Status)
+        assert!(heartbeats.len() > 0);
+        let latest = heartbeats[0];
+        Ok(Status {
+               last_heartbeat_received: latest.datetime.to_rfc3339(),
+               soc1: latest.soc1,
+               soc2: latest.soc2,
+           })
     }
 }

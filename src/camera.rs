e476325c2d1e40f45c023650ea7c790bd2068305
Pete Gadomski <pete.gadomski@gmail.com>
2017-08-25 08:58:19 -0600

    Canonicalize all the paths

--- src/camera.rs
+++ src/camera.rs
@@ -7,7 +7,7 @@
 use {Error, Result};
 use chrono::{DateTime, TimeZone, Utc};
 use std::ffi::OsString;
-use std::fs::{DirEntry, ReadDir};
+use std::fs::ReadDir;
 use std::path::{Path, PathBuf};
 use url::Url;
 
@@ -17,7 +17,7 @@ const DEFAULT_SERVER_BASE_URL: &'static str = "http://iridiumcam.lidar.io";
 /// A remote camera, usually used to take pictures of glaciers or other cool stuff.
 #[derive(Debug)]
 pub struct Camera {
-    directory: PathBuf,
+    path: PathBuf,
     extensions: Vec<OsString>,
 }
 
@@ -50,17 +50,21 @@ pub struct Server {
 impl Camera {
     /// Creates a new camera with a local image path.
     ///
+    /// The local image path is canonicalized.
+    ///
     /// # Examples
     ///
     /// ```
+    /// use std::path::Path;
     /// # use glacio::Camera;
-    /// let camera = Camera::new("data/ATLAS_CAM");
+    /// let camera = Camera::new("data/ATLAS_CAM").unwrap();
+    /// assert_eq!(Path::new("data/ATLAS_CAM").canonicalize().unwrap(), camera.path());
     /// ```
-    pub fn new<P: AsRef<Path>>(path: P) -> Camera {
-        Camera {
-            directory: path.as_ref().to_path_buf(),
-            extensions: DEFAULT_EXTENSIONS.iter().map(|&s| s.into()).collect(),
-        }
+    pub fn new<P: AsRef<Path>>(path: P) -> Result<Camera> {
+        Ok(Camera {
+               path: path.as_ref().canonicalize()?,
+               extensions: DEFAULT_EXTENSIONS.iter().map(|&s| s.into()).collect(),
+           })
     }
 
     /// Returns an iterator over this camera's images.
@@ -69,11 +73,11 @@ impl Camera {
     ///
     /// ```
     /// # use glacio::Camera;
-    /// let camera = Camera::new("data/ATLAS_CAM");
+    /// let camera = Camera::new("data/ATLAS_CAM").unwrap();
     /// let images = camera.images().unwrap().collect::<Vec<_>>();
     /// ```
     pub fn images(&self) -> Result<Images> {
-        self.directory
+        self.path
             .read_dir()
             .map(|read_dir| {
                      Images {
@@ -82,6 +86,19 @@ impl Camera {
                      }
                  })
             .map_err(Error::from)
+    }
+
+    /// Returns this camera's path.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use glacio::Camera;
+    /// let camera = Camera::new("data/ATLAS_CAM").unwrap();
+    /// let path = camera.path();
+    /// ```
+    pub fn path(&self) -> &Path {
+        &self.path
     }
 }
 
@@ -108,26 +125,30 @@ impl Iterator for Images {
 impl Image {
     /// Creates a new image from a path.
     ///
+    /// The path is canonicalized.
+    ///
     /// # Examples
     ///
     /// ```
+    /// use std::path::Path;
     /// # use glacio::Image;
     /// let image = Image::new("data/ATLAS_CAM/ATLAS_CAM_20170806_152500.jpg").unwrap();
+    /// assert_eq!(Path::new("data/ATLAS_CAM/ATLAS_CAM_20170806_152500.jpg").canonicalize().unwrap(),
+    ///            image.path());
     /// ```
     pub fn new<P: AsRef<Path>>(path: P) -> Result<Image> {
-        if let Some(file_stem) = path.as_ref().file_stem().and_then(|file_stem| {
-                                                                        file_stem.to_str()
-                                                                    }) {
+        let path = path.as_ref().canonicalize()?;
+        if let Some(file_stem) = path.file_stem().and_then(|file_stem| file_stem.to_str()) {
             if file_stem.len() <= 15 {
                 Err(Error::ImageFilename(format!("File stem {} is too short", file_stem)))
             } else {
                 let (_, s) = file_stem.split_at(file_stem.len() - 15);
                 Utc.datetime_from_str(s, "%Y%m%d_%H%M%S")
                     .map_err(Error::from)
-                    .map(|datetime| Image { datetime: datetime, path: path.as_ref().to_path_buf()})
+                    .map(|datetime| Image { datetime: datetime, path: path.clone() })
             }
         } else {
-            Err(Error::ImageFilename(format!("No file stem found for path: {:?}", path.as_ref())))
+            Err(Error::ImageFilename(format!("No file stem found for path: {:?}", path)))
         }
     }
 
@@ -148,17 +169,21 @@ impl Image {
 impl Server {
     /// Creates a new server.
     ///
+    /// The server document root is canonicalized.
+    ///
     /// # Examples
     ///
     /// ```
+    /// use std::path::Path;
     /// # use glacio::camera::Server;
-    /// let server = Server::new("data");
+    /// let server = Server::new("data").unwrap();
+    /// assert_eq!(Path::new("data").canonicalize().unwrap(), server.document_root());
     /// ```
-    pub fn new<P: AsRef<Path>>(document_root: P) -> Server {
-        Server {
-            document_root: document_root.as_ref().to_path_buf(),
-            base_url: Url::parse(DEFAULT_SERVER_BASE_URL).unwrap(),
-        }
+    pub fn new<P: AsRef<Path>>(document_root: P) -> Result<Server> {
+        Ok(Server {
+               document_root: document_root.as_ref().canonicalize()?,
+               base_url: Url::parse(DEFAULT_SERVER_BASE_URL).unwrap(),
+           })
     }
 
     /// Returns the url for the provided image.
@@ -168,12 +193,25 @@ impl Server {
     /// ```
     /// # use glacio::camera::{Image, Server};
     /// let image = Image::new("data/ATLAS_CAM/ATLAS_CAM_20170806_152500.jpg").unwrap();
-    /// let server = Server::new("data");
+    /// let server = Server::new("data").unwrap();
     /// let url = server.url_for(&image).unwrap();
     /// ```
     pub fn url_for(&self, image: &Image) -> Result<Url> {
         let input = image.path().strip_prefix(&self.document_root)?;
         self.base_url.join(&input.to_string_lossy()).map_err(Error::from)
+    }
+
+    /// Returns this server's document root.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use glacio::camera::Server;
+    /// let server = Server::new("data").unwrap();
+    /// let document_root = server.document_root();
+    /// ```
+    pub fn document_root(&self) -> &Path {
+        &self.document_root
     }
 }
 
@@ -183,12 +221,12 @@ mod tests {
 
     #[test]
     fn new_camera() {
-        Camera::new("data/ATLAS_CAM");
+        Camera::new("data/ATLAS_CAM").unwrap();
     }
 
     #[test]
     fn camera_images() {
-        let camera = Camera::new("data/ATLAS_CAM");
+        let camera = Camera::new("data/ATLAS_CAM").unwrap();
         let images = camera.images().unwrap();
         assert_eq!(1, images.count());
 
@@ -199,8 +237,36 @@ mod tests {
 
     #[test]
     fn server_url() {
-        let server = Server::new("data");
-        let camera = Camera::new("data/ATLAS_CAM");
+        let server = Server::new("data").unwrap();
+        let camera = Camera::new("data/ATLAS_CAM").unwrap();
+        let image = camera.images()
+            .unwrap()
+            .next()
+            .unwrap()
+            .unwrap();
+        let url = server.url_for(&image).unwrap();
+        assert_eq!("http://iridiumcam.lidar.io/ATLAS_CAM/ATLAS_CAM_20170806_152500.jpg",
+                   url.as_str());
+    }
+
+    #[test]
+    fn server_url_subdirectory() {
+        let server = Server::new(Path::new("data").canonicalize().unwrap()).unwrap();
+        let camera = Camera::new("data/HEL_BERGCAM3/StarDot1").unwrap();
+        let image = camera.images()
+            .unwrap()
+            .next()
+            .unwrap()
+            .unwrap();
+        let url = server.url_for(&image).unwrap();
+        assert_eq!("http://iridiumcam.lidar.io/HEL_BERGCAM3/StarDot1/HEL_BERGCAM3_StarDot1_20170825_120000.jpg",
+                   url.as_str());
+    }
+
+    #[test]
+    fn server_url_mixing_absolute_and_relative() {
+        let server = Server::new("data").unwrap();
+        let camera = Camera::new("data/ATLAS_CAM").unwrap();
         let image = camera.images()
             .unwrap()
             .next()

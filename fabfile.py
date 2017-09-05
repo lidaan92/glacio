25d37d95bc81b72407b2150b821e268935639e62
Pete Gadomski <pete.gadomski@gmail.com>
2017-09-05 08:55:17 -0600

    Add fabfile for deploying

---
+++ fabfile.py
@@ -0,0 +1,27 @@
+from fabric.api import sudo, cd, task, run, local, env
+
+REMOTE_DIRECTORY = "/var/www/glacio"
+
+env.hosts = ["lidar.io"]
+env.use_ssh_config = True
+
+@task
+def deploy():
+    push()
+    update()
+    restart()
+
+@task
+def push():
+    local("git push")
+
+@task
+def update():
+    with cd(REMOTE_DIRECTORY):
+        run("git pull")
+        run("cargo build --release")
+        run("cargo test")
+
+@task
+def restart():
+    sudo("supervisorctl restart glacio-api")

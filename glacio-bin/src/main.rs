bc791b96deb2c70fc297a9704fd8f58de9e2d490
Pete Gadomski <pete.gadomski@gmail.com>
2017-11-02 06:34:21 -0600

    Convert glacio-bin to use clap

--- glacio-bin/src/main.rs
+++ glacio-bin/src/main.rs
@@ -1,42 +1,24 @@
-extern crate docopt;
+#[macro_use]
+extern crate clap;
 extern crate env_logger;
 extern crate glacio_http;
 extern crate iron;
-#[macro_use]
-extern crate serde_derive;
-
-use docopt::Docopt;
-use glacio_http::{Api, Config};
-use iron::Iron;
-
-const USAGE: &'static str = "
-Glacier research data collection and dissemination.
-
-Usage:
-    glacio api <config> <addr>
-    glacio heartbeats <config>
-
-Options:
-    -h --help           Show this screen.
-";
-
-#[derive(Debug, Deserialize)]
-struct Args {
-    cmd_api: bool,
-    cmd_heartbeats: bool,
-    arg_addr: String,
-    arg_config: String,
-}
 
 fn main() {
+    use glacio_http::{Api, Config};
+    use iron::Iron;
+    use clap::App;
+
     env_logger::init().unwrap();
-    let args: Args = Docopt::new(USAGE).and_then(|d| d.deserialize()).unwrap_or_else(|e| e.exit());
-    if args.cmd_api {
-        let api = Api::from_path(args.arg_config).unwrap();
-        println!("Serving glacio api on http://{}", args.arg_addr);
-        Iron::new(api).http(args.arg_addr).unwrap();
-    } else if args.cmd_heartbeats {
-        let config = Config::from_path(args.arg_config).unwrap();
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+    if let Some(matches) = matches.subcommand_matches("api") {
+        let api = Api::from_path(matches.value_of("CONFIG").unwrap()).unwrap();
+        let addr = matches.value_of("ADDR").unwrap();
+        println!("Serving glacio api on http://{}", addr);
+        Iron::new(api).http(addr).unwrap();
+    } else if let Some(matches) = matches.subcommand_matches("heartbeats") {
+        let config = Config::from_path(matches.value_of("CONFIG").unwrap()).unwrap();
         for heartbeat in config.atlas.read_sbd().unwrap() {
             println!("{:?}", heartbeat);
         }
